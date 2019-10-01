fb614df8f9b394895583bc61fa3e6773f69c75d7
sethripman <sethripman@gmail.com>
2019-10-01 10:45:52 -0700

    Tracking yes / no score

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 import { isYes } from './isYes.js';
 const myButton = document.getElementById('action-button');
-const numberCorrect = 0;
 
 myButton.onclick = () => {
+    // Declare correct answer tracker
+    let numberCorrect = 0;
+
     alert('Welcome to my quiz!');
 
     // Return if they don't want to take the quiz
     const shouldProceed = confirm('Are you sure about this?');
+    if (!shouldProceed)
+        return;
     
     // Question One - correct answer is yes
     const userAnswerOne = prompt('Lightning: does it ever bring Seth down? (yes/no)');
     const sanitizedUserAnswerOne = userAnswerOne.toLowerCase().trim();
-    isYes(sanitizedUserAnswerOne);
+    if (isYes(sanitizedUserAnswerOne))
+        numberCorrect++;
 
     // Question Two - correct answer is no
     const userAnswerTwo = prompt('Is Seth ignorant and ambivalent about the effectiveness of Toad Style? (yes/no)');
     const sanitizedUserAnswerTwo = userAnswerTwo.toLowerCase().trim();
-    isYes(sanitizedUserAnswerTwo);
-
+    if (!isYes(sanitizedUserAnswerOne))
+        numberCorrect++;
+    
     // Question Three - correct answer is yes
     const userAnswerThree = prompt('JEANS? (yes/no)');
     const sanitizedUserAnswerThree = userAnswerThree.toLowerCase().trim();
-    isYes(sanitizedUserAnswerThree);
-
-
+    if (isYes(sanitizedUserAnswerOne))
+        numberCorrect++;
 
 };
 
