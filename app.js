4989c1616933bad0b705599f17acdb96acb3a1ef
sethripman <sethripman@gmail.com>
2019-10-01 11:13:19 -0700

    Added name tracking

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ import { isYes } from './isYes.js';
 const myButton = document.getElementById('action-button');
 
 myButton.onclick = () => {
-    // Declare correct answer tracker
+    // Declare correct answer tracker and name string
     let numberCorrect = 0;
 
+    // Start the Quiz
     alert('Welcome to my quiz!');
 
+    // Ask their name and store the variable
+    let name = prompt('What is your name?');
+
     // Return if they don't want to take the quiz
-    const shouldProceed = confirm('Are you sure about this?');
+    const shouldProceed = confirm(name + ', are you sure about this?');
     if (!shouldProceed)
         return;
     
@@ -21,19 +25,33 @@ myButton.onclick = () => {
     // Question Two - correct answer is no
     const userAnswerTwo = prompt('Is Seth ignorant and ambivalent about the effectiveness of Toad Style? (yes/no)');
     const sanitizedUserAnswerTwo = userAnswerTwo.toLowerCase().trim();
-    if (!isYes(sanitizedUserAnswerOne))
+    if (!isYes(sanitizedUserAnswerTwo))
         numberCorrect++;
     
     // Question Three - correct answer is yes
-    const userAnswerThree = prompt('JEANS? (yes/no)');
+    const userAnswerThree = prompt('Now the big one, ' + name + '... JEANS? (yes/no)');
     const sanitizedUserAnswerThree = userAnswerThree.toLowerCase().trim();
-    if (isYes(sanitizedUserAnswerOne))
+    if (isYes(sanitizedUserAnswerThree))
         numberCorrect++;
 
-};
+    // Tell the user how many they got correct with percentage
+    alert('Well done ' + name + '! Check the bottom of the page for your results.');
 
-// reveal the hidden paragraph
-function revealResults() {
+    // reveal the hidden paragraph and adjust size based on number correct  
     const hideMe = document.getElementById('surprise');
     hideMe.classList.remove('hidden');
-}
+
+    // Add the user's name to the results
+    const nameSpan = document.getElementById('name-value');
+    nameSpan.textContent = name;
+    
+    /* Stretch Goal - change the size of the hidden paragraph based on the result
+    if (numberCorrect === 1)
+        hideMe.classList.replace ('result-size', 'result-size-good');
+    if (numberCorrect === 2)
+        hideMe.classList.replace ('result-size', 'result-size-great');
+    if (numberCorrect === 3)
+        hideMe.classList.replace ('result-size', 'result-size-perfect');
+    */
+
+};
